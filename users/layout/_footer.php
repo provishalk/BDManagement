07b2fd18073d9859528ab029158035e8f0ec59b2
vishal sharma <provishalk@gmail.com>
2021-05-11 09:12:44 +0530

    first commit

---
+++ users/layout/_footer.php
@@ -0,0 +1,16 @@
+<footer class="nav navbar-fixed-bottom">
+        <div style="display: flex;">
+            <div style="margin-left: 20px;">
+                <p>Plasma Helpline No. <strong>+91-9587458745 +91-8745858585 +91-8747874874</strong></p>
+            </div>
+            <div style="margin-left: auto; margin-right: 20px;">
+                <p>COVID-19 Helpline No. <strong>011-46300000</strong></p> 
+            </div>
+            
+        </div>
+</footer>
+
+
+
+</body>
+</html>
